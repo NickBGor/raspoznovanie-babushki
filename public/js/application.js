f23e8774dd857742d87d559f434a30b48144cf1c
VanMexico <ivan@pitertsev.pw>
2020-09-23 23:13:04 +0300

    Update home page and speech

--- public/js/application.js
+++ public/js/application.js
@@ -1,20 +1,59 @@
+const { speechSynthesis } = window
 const form = document.querySelector('#send-image')
-const readBtn = document.querySelector('#read-btn')
-const stopBtn = document.querySelector('#cancel-read')
-let text = ''
-const textContainer = document.querySelector('#text-container')
-const synth = window.speechSynthesis
+const playBtn = document.querySelector('.play')
+const stopBtn = document.querySelector('.stop')
+const voicesSelect = document.querySelector('.voices')
+const textArea = document.querySelector('.text')
 
-function speak(text) {
-  const message = new SpeechSynthesisUtterance()
-  message.lang = 'ru-RU'
-  message.text = text
-  synth.speak(message)
+let text = ''
+
+let voices = []
+
+function showElement(element) {
+  element.classList.add('visible')
+  element.classList.remove('hidden')
+}
+
+function hideElement(element) {
+  element.classList.add('hidden')
+  element.classList.remove('visible')
+}
+
+textArea.addEventListener('input', () => {
+  if (textArea.value !== '') {
+    showElement(playBtn)
+  } else {
+    hideElement(playBtn)
+    hideElement(stopBtn)
+  }
+})
+
+function generateVoices() {
+  voices = speechSynthesis.getVoices()
+
+  const voiceList = voices
+    .map((voice, index) => voice.lang === 'ru-RU' && `<option value="${index}">${voice.name} ${voice.lang}</option>`)
+    .join('')
+
+  voicesSelect.innerHTML = voiceList
+}
+
+generateVoices()
+speechSynthesis.addEventListener('voiceschanged', generateVoices)
+
+function speak(content) {
+  if (speechSynthesis.speaking) {
+    console.log('Текст уже воспроизводиться!')
+    return
+  }
+  const speech = new SpeechSynthesisUtterance(content)
+  speech.voice = voices[voicesSelect.value]
+  speechSynthesis.speak(speech)
 }
 
 form.addEventListener('submit', async (e) => {
   e.preventDefault()
-  textContainer.innerHTML = ''
+  textArea.value = ''
   const file = document.getElementById('file').files[0]
   if (!file) return
 
@@ -29,19 +68,27 @@ form.addEventListener('submit', async (e) => {
   })
 
   const answer = await response.json()
-  text = answer.text.split('\n')
-  text.forEach((el) => {
-    textContainer.innerHTML += `${el}<br>`
+  const content = answer.text.split('\n')
+
+  content.forEach((el) => {
+    text += `${el}\n`
   })
+
+  textArea.value = text
+
   form.reset()
 })
 
-readBtn.addEventListener('click', () => {
-  speak(text)
+playBtn.addEventListener('click', () => {
+  speak(textArea.value)
+  if (stopBtn.classList.contains('hidden')) {
+    showElement(stopBtn)
+  }
 })
 
 stopBtn.addEventListener('click', () => {
-  synth.cancel()
+  speechSynthesis.cancel()
+  if (stopBtn.classList.contains('visible')) {
+    hideElement(stopBtn)
+  }
 })
-
-module.exports = speak
